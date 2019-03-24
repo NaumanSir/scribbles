2651a8c345cd185e264b356b0129abff206e1f6a
Nauman Husnani <naumanhusnani@gmail.com>
2019-03-24 15:21:17 -0500

    attempted to use react-icons, but no module found

--- src/note.js
+++ src/note.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import FaPencil from 'react-icons/lib/fa/pencil';
+import FaTrash from 'react-icons/lib/fa/trash';
 
 class Note extends React.Component {
     render() {
@@ -6,8 +8,8 @@ class Note extends React.Component {
             <div className="note">
                 <p>Note for bulletin board</p>
                 <span>
-                    <button>Edit</button>
-                    <button>Remove</button>
+                    <button><FaPencil /></button>
+                    <button><FaTrash /></button>
                 </span>
             </div>
         )
