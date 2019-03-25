b3b34ebf6aa549ecf0dd29108455c2ad5d9ad4b1
Nauman Husnani <naumanhusnani@gmail.com>
2019-03-25 14:08:38 -0500

    added events

--- src/note.js
+++ src/note.js
@@ -3,13 +3,26 @@ import React from 'react';
 // import FaTrash from 'react-icons/lib/fa/trash';
 
 class Note extends React.Component {
+    constructor(props) {
+        super(props);
+        this.edit = this.edit.bind(this);
+        this.delete = this.delete.bind(this);
+    }
+    edit() {
+        alert('editing note')
+    };
+
+    delete() {
+        alert('deleting note')
+    };
+
     render() {
         return(
             <div className="note">
                 <p>Note for bulletin board</p>
                 <span>
-                    <button>Edit</button>
-                    <button>Delete</button>
+                    <button onClick={this.edit} id="edit">Edit</button>
+                    <button onClick={this.delete} id="delete">Delete</button>
                     {/* <button><FaPencil /></button> */}
                     {/* <button><FaTrash /></button> */}
                 </span>
